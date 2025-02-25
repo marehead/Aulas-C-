55ea4ee3388aaa7ec8a8d81aa9c85742cfa78962
MARE LOPES DOS SANTOS <mare.lsantos@senacsp.edu.br>
2025-02-25 17:05:28 -0300

    Commit inicial do projeto C#

---
+++ CSHARP.cs
@@ -0,0 +1,24 @@
+namespace Aula01
+{
+using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        int idade = 23;
+        string nomeCompleto = "Mare dos Santos";
+
+        var variavelSemTipo = "Mare";
+
+        Console.WriteLine(variavelSemTipo.GetType());
+        Console.WriteLine("Digite seu nome:")
+        string nome/usuario = Console.readLine();
+
+        Console WriteLine(sbyte"Meu nome é: (nomeCompleto) e a minha idade é (idade)");
+
+        variavelSemTipo =  15;
+    }
+}
+}
